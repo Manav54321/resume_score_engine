864f452d17d52ab2b0b3fd26c0f091473ae31e5e
manav <manavdesai170303@gmail.com>
2025-08-19 22:03:37 +0530

    updated/final r-s-e

--- resume-score-engine/backend/app/main.py
+++ resume-score-engine/backend/app/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Resume ATS Analyzer API")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    # "https://your-frontend-url.vercel.app", 
+    "https://resume-score-engine-4ga7wcnuo-manav-desais-projects.vercel.app/", 
 ]
 
 app.add_middleware(
